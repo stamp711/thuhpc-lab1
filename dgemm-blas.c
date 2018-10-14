6ff37e27831c081e8b47e0feb3e865d9e7413c6f
Apricity <stamp1024@gmail.com>
2018-10-15 00:43:25 +0800

    format code

--- dgemm-blas.c
+++ dgemm-blas.c
@@ -1,14 +1,14 @@
 #define DGEMM dgemm_
-extern void DGEMM (char*, char*, int*, int*, int*, double*, double*, int*, double*, int*, double*, double*, int*); 
+extern void DGEMM(char *, char *, int *, int *, int *, double *, double *, int *, double *, int *, double *, double *, int *);
 
-const char* dgemm_desc = "Reference dgemm.";
+const char *dgemm_desc = "Reference dgemm.";
 
 /* This routine performs a dgemm operation
  *  C := C + A * B
  * where A, B, and C are lda-by-lda matrices stored in column-major format.
  * On exit, A and B maintain their input values.    
  * This function wraps a call to the BLAS-3 routine DGEMM, via the standard FORTRAN interface - hence the reference semantics. */
-void square_dgemm (int N, double* A, double* B, double* C)
+void square_dgemm(int N, double *A, double *B, double *C)
 {
   char TRANSA = 'N';
   char TRANSB = 'N';
@@ -20,4 +20,4 @@ void square_dgemm (int N, double* A, double* B, double* C)
   int LDB = N;
   int LDC = N;
   DGEMM(&TRANSA, &TRANSB, &M, &N, &K, &ALPHA, A, &LDA, B, &LDB, &BETA, C, &LDC);
-}   
+}
